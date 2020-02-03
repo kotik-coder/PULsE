af6d4253e61dff00ee8358e34058af2b680dce76
Artem Lunev <artem.lunev@ukaea.uk>
2020-02-03 10:44:34 +0000

    PULsE v.1.5_03 (cleaning)

--- PuLSE/README.txt
+++ PuLSE/README.txt
@@ -1,5 +1,5 @@
 PULsE 
 (Processing Unit for Laser flaSh Experiments)
-v. 1.5_02
+v. 1.5_03
 
 Please see manual for detailed instructions
--- PuLSE/src/pulse/ui/charts/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * A package that is responsible for (a) plotting charts to display and interact with data from
- *  {@code HeatingCurve} objects; and (b) plotting preview charts from {@code ResultTable}s.
- */
-
-package pulse.ui.charts;
