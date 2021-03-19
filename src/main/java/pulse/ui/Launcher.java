2d45dca90e03a059babe873769f78b178d6e0af3
Artem Lunev <artem.lunev@ukaea.uk>
2021-03-19 12:03:08 +0100

    debug false

--- src/main/java/pulse/ui/Launcher.java
+++ src/main/java/pulse/ui/Launcher.java
@@ -34,7 +34,7 @@ public class Launcher {
 
 	private PrintStream errStream;
 	private File errorLog;
-	private final static boolean DEBUG = true;
+	private final static boolean DEBUG = false;
 
 	private Launcher() {
 		arrangeErrorOutput();
