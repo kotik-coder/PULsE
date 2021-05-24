25c064293e51d96edc1dc744f6d766ffaae72c36
Artem Lunev <artem.lunev@ukaea.uk>
2021-05-24 21:54:18 +0200

    Additional changes to the reader

--- src/main/java/pulse/io/readers/PulseDataReader.java
+++ src/main/java/pulse/io/readers/PulseDataReader.java
@@ -5,8 +5,18 @@ import java.io.IOException;
 
 import pulse.problem.laser.NumericPulseData;
 
+/**
+ * A reader for importing numeric pulse data -- if available.
+ *
+ */
+
 public interface PulseDataReader extends AbstractReader<NumericPulseData> {
 
+	
+	/**
+	 * Converts the ASCII file to a {@code NumericPulseData} object.
+	 */
+	
 	@Override
 	public abstract NumericPulseData read(File file) throws IOException;
 
