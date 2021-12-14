517b9134336f8e47a4b241aa7268b89d18a642ce
kotik-coder <artem.v.lunev@gmail.com>
2021-12-14 16:09:12 +0100

    Changes to NumericProperty.xml and string constants in messages.properties
    Changed min/max values of some properties. Fixed the 'default-search-variable'
    attribute not appearing where it should have appeared, and being present
    where it is not required. 
    
    Added some string constants.

--- src/main/java/pulse/ui/Messages.java
+++ src/main/java/pulse/ui/Messages.java
@@ -6,18 +6,19 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "messages";
 
-	private static final ResourceBundle RESOURCE_BUNDLE = getBundle(BUNDLE_NAME);
+    private static final String BUNDLE_NAME = "messages";
 
-	private Messages() {
-	}
+    private static final ResourceBundle RESOURCE_BUNDLE = getBundle(BUNDLE_NAME);
 
-	public static String getString(String key) {
-		try {
-			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
-			return '!' + key + '!';
-		}
-	}
+    private Messages() {
+    }
+
+    public static String getString(String key) {
+        try {
+            return RESOURCE_BUNDLE.getString(key);
+        } catch (MissingResourceException e) {
+            return '!' + key + '!';
+        }
+    }
 }
